e04350858248646711feb1f1a614fbe7fe62576e
Itai Tagar <itait@mews5308>
2018-03-25 18:53:41 +0300

    Cosmetic changes

--- SecretWallet.py
+++ SecretWallet.py
@@ -34,6 +34,8 @@ TITLE_IMAGE = "Title.png"
 STORE_IMAGE = "Store.png"
 RETRIEVE_IMAGE = "Retrieve.png"
 SUB_TITLE_IMAGE = "SubTitle.png"
+ABOUT_IMAGE = "About.png"
+HELP_IMAGE = "Help.png"
 
 BAD_STATUS_COLOR = 'red'
 GOOD_STATUS_COLOR = 'green'
@@ -48,18 +50,24 @@ class ClientGUI:
 
     def __init__(self, master, client):
         """ Initialize the GUI window for the client. """
+        # Set the data members: master is the root window and client is
+        # the Client object that is associated with this window.
         self.__master = master
         self.__client = client
+        # Set GUI global parameters and create the GUI window.
         self.__status = tk.Label(master, text=DEFAULT_STATUS)
         self.__create_window()
         self.__master.protocol("WM_DELETE_WINDOW", self.__on_close)
+        # Run the GUI loop.
         self.__master.mainloop()
 
     # ==----   GUI Creation Functions   ----== #
 
     def __create_title(self):
         """ Creates the title of the main window. """
+        # Set the title of the window.
         self.__master.title(GUI_TITLE + " " + VERSION_TITLE)
+        # Set the title image inside the window itself.
         title_frame = tk.Frame(self.__master)
         title_image = tk.PhotoImage(file=TITLE_IMAGE)
         title = tk.Label(title_frame, image=title_image)
@@ -69,6 +77,7 @@ class ClientGUI:
 
     def __create_sub_title(self):
         """ Creates the secondary title of the main window. """
+        # Set the subtitle image inside the window itself.
         sub_title_frame = tk.Frame(self.__master)
         sub_title_image = tk.PhotoImage(file=SUB_TITLE_IMAGE)
         sub_title = tk.Label(sub_title_frame, image=sub_title_image)
@@ -78,14 +87,9 @@ class ClientGUI:
 
     def __create_status_bar(self):
         """ Creates the status bar of the GUI. """
+        # The status bar is a simple Label with a sunken attribute.
         self.__status.config(bd=1, relief=tk.SUNKEN, anchor=tk.W)
         self.__status.pack(side=tk.BOTTOM, fill=tk.X)
-
-    @staticmethod
-    def __top_secret():
-        """ Activates Top Secret function. """
-        file_path = os.getcwd() + os.path.sep + "TopSecret.mp3"
-        subprocess.Popen(r'vlc ' + file_path, shell=True)
 
     def __create_menu(self):
         """ Creates the top main menu of the GUI. """
@@ -124,18 +128,20 @@ class ClientGUI:
         store_icon = tk.PhotoImage(file=STORE_IMAGE).subsample(2, 2)
         retrieve_icon = tk.PhotoImage(file=RETRIEVE_IMAGE).subsample(2, 2)
 
+        # Store Button.
         store_button = tk.Button(button_bar, command=self.__store)
         store_button.config(image=store_icon)
         store_button.image = store_icon
         # Mouse hover.
-        store_button.bind('<Enter>', lambda event: self.__status.config(text='Store'))
+        store_button.bind('<Enter>', lambda event: self.__status.config(text=STORE_TITLE))
         store_button.bind('<Leave>', lambda event: self.__reset_status())
 
+        # Retrieve Button.
         retrieve_button = tk.Button(button_bar, command=self.__retrieve)
         retrieve_button.config(image=retrieve_icon)
         retrieve_button.image = retrieve_icon
         # Mouse hover.
-        retrieve_button.bind('<Enter>', lambda event: self.__status.config(text='Retrieve'))
+        retrieve_button.bind('<Enter>', lambda event: self.__status.config(text=RETRIEVE_TITLE))
         retrieve_button.bind('<Leave>', lambda event: self.__reset_status())
 
         store_button.pack(side=tk.LEFT, padx=100, pady=15)
@@ -167,6 +173,9 @@ class ClientGUI:
         self.__status.after(STATUS_TIMEOUT, self.__reset_status)
 
     def __store(self):
+        """ Function that handles the Store procedure when the Store
+        button is pressed. The procedure creates a new window for Store action,
+        and is interacting with the Client's store methods. """
         store_window = tk.Toplevel()
         store_window.title(STORE_TITLE)
         store_window.resizable(False, False)
@@ -196,6 +205,7 @@ class ClientGUI:
         info_label.pack(fill=tk.X, expand=tk.YES)
 
         def store_command(transaction_id, key, value):
+            """ Store data by Client. """
             # Reset entries highlight.
             transaction_entry.config(highlightbackground="grey")
             key_entry.config(highlightbackground="grey")
@@ -242,6 +252,9 @@ class ClientGUI:
         cancel_button.pack()
 
     def __retrieve(self):
+        """ Function that handles the Store procedure when the Retrieve
+        button is pressed. The procedure creates a new window for Retrieve
+        action, and is interacting with the Client's retrieve methods. """
         retrieve_window = tk.Toplevel()
         retrieve_window.title(RETRIEVE_TITLE)
         retrieve_window.resizable(False, False)
@@ -273,6 +286,7 @@ class ClientGUI:
         info_label.pack(fill=tk.X, expand=tk.YES)
 
         def retrieve_command(transaction_id, key):
+            """ Retrieve data by Client. """
             # Reset entries highlight.
             transaction_entry.config(highlightbackground="grey")
             key_entry.config(highlightbackground="grey")
@@ -316,13 +330,18 @@ class ClientGUI:
         store_button.pack()
         cancel_button.pack()
 
+    def __on_close(self):
+        """ A function which handles the exit procedure. """
+        self.__client.exit()
+        self.__master.destroy()
+
     @staticmethod
     def __help():
         """ Opens help window with information to the user. """
         info_window = tk.Toplevel()
         info_window.resizable(width=False, height=False)
-        info_window.title("Help")
-        info_image = tk.PhotoImage(file="Help.png")
+        info_window.title(HELP_TITLE)
+        info_image = tk.PhotoImage(file=HELP_IMAGE)
         info_msg = tk.Label(info_window, image=info_image)
         info_msg.image = info_image  # Required for displaying the image.
         info_msg.pack()
@@ -332,16 +351,18 @@ class ClientGUI:
         """ Opens about window with information about the application. """
         about_window = tk.Toplevel()
         about_window.resizable(width=False, height=False)
-        about_window.title("About")
-        about_image = tk.PhotoImage(file="About.png")
+        about_window.title(ABOUT_TITLE)
+        about_image = tk.PhotoImage(file=ABOUT_IMAGE)
         info_msg = tk.Label(about_window, image=about_image)
         info_msg.image = about_image  # Required for displaying the image.
         info_msg.pack()
 
-    def __on_close(self):
-        """ A function which handles the exit procedure. """
-        self.__client.exit()
-        self.__master.destroy()
+    @staticmethod
+    def __top_secret():
+        """ Activates Top Secret function. """
+        file_path = os.getcwd() + os.path.sep + "TopSecret.mp3"
+        subprocess.Popen(r'vlc ' + file_path, shell=True)
+
 
 
 if __name__ == '__main__':
